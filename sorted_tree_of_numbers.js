d18e350bf492af0eb22497ed52ac20b1dbbbe6a0
Source Academy User <No public email provided>
2022-11-09 13:19:07 +0800

    Changes made from Source Academy

---
+++ sorted_tree_of_numbers.js
@@ -0,0 +1,158 @@
+
+function insert(x, xs) {
+    if(is_null(xs)) {
+        return list(x);
+    }
+    else if (is_null(head(xs))) {
+        return pair(head(xs),insert(x,tail(xs)));
+    }       
+    else if (x<=head(xs)) {
+        return pair(x,xs);
+    }       
+   else {
+       return pair(head(xs),insert(x,tail(xs)));
+   }
+    
+}
+
+function insertion_sort(xs) {
+    return is_null(xs)
+           ? xs
+           : insert(head(xs), insertion_sort(tail(xs)));
+}
+
+function map_tree(fun, tree) {
+    return map(sub_tree =>
+                   !is_list(sub_tree)
+                   ? fun(sub_tree)
+                   : map_tree(fun, sub_tree),
+               tree);
+}
+
+
+function flatten(xs) {
+    if (is_null(xs)) {
+        return null;
+    }
+
+    else if (!is_pair(head(xs))) {
+        return pair(head(xs),flatten(tail(xs)));
+    }
+    
+    else {
+        return append(flatten(head(xs)),flatten(tail(xs)));
+    }
+    
+    
+}
+
+function find_p(xs) {
+    if (is_null(xs)) {
+        return false;
+    }
+   else if (!is_list(head(xs))) {
+       return head(xs)==="p" ? true : find_p(tail(xs));
+   } 
+   else {
+       return find_p(head(xs)) ? true : find_p(tail(xs));
+   }
+       
+}      
+       
+
+function insert_tree(xs,y) {
+    if(is_null(xs)) {
+        return null;
+    }
+   else if (!is_list(head(xs))) {
+       return head(xs)==="p" ? pair(y,tail(xs)) : pair(head(xs),insert_tree(tail(xs),y));
+   }
+   
+   else {
+      return find_p(head(xs)) ? pair(insert_tree(head(xs),y),tail(xs)) :
+        pair(head(xs),insert_tree(tail(xs),y));
+       
+       
+   } 
+    
+    
+}
+ 
+function find_null(xs,y,s) {
+    if (is_null(xs)) {
+        return s;
+    }
+    else {
+        return head(xs)===null ? find_null(tail(xs),y+1,pair(y,s)) : 
+            find_null(tail(xs),y+1,s);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function make_SToN(T) { 
+    //const tree = T;
+    let s = map_tree(x=>"p",T);
+    //display_list(s);
+    let numbers = insertion_sort(flatten(T));
+    //display_list(numbers);
+    const len = length(numbers);
+    for(let i=0;i<len;i=i+1) {
+        s = insert_tree(s,head(numbers));
+        numbers = tail(numbers);
+        
+        
+    }
+    
+    return s;
+    
+    
+    
+    
+    
+
+    // WRITE YOUR SOLUTION HERE.
+
+} 
+const my_tree = list( list(23, 18, list(20, 11), 25), 17, list(16), 14, 24, 15,
+                      list( list(13), 21, list(22, 12, list(19, 15)) ) );  
+                      
+                      
+const y = flatten(my_tree);
+const d = map_tree(x=>"p",my_tree);
+//display_list(d);
+//display_list(my_tree);              
+//display_list(make_SToN(my_tree));  
+
+
+const x = list(1,2,null,6,null,5);
+find_null(x,0,null);
+
+                      
+
+    
