a843292adc6e977e7c35ad1521159e806900bdee
Source Academy User <No public email provided>
2022-11-12 13:02:44 +0800

    Changes made from Source Academy

--- array_tree_map.js
+++ array_tree_map.js
@@ -21,5 +21,65 @@ function array_treemap(f,A) {
 }
 
 const T = [2, 3, [1, [4, 5], []], 6, [7]];
-const S = array_treemap(x => x + 1, T);
-S;
+const S = array_treemap(x => x * 2, T);
+
+
+
+
+function append_array(xs,ys) {
+    const len = array_length(ys);
+    for(let i=0;i<len;i=i+1) {
+        xs[array_length(xs)] = ys[i];
+    }
+
+    
+}
+function flatten_array(A) {
+    const len = array_length(A);
+    const result = [];
+    for (let i=0;i<len;i=i+1) {
+        if(is_array(A[i])) {
+           append_array(result,flatten_array(A[i])); 
+            
+        }
+    
+        else {
+            result[array_length(result)]=A[i];
+        }
+        
+    }
+   return result;
+}
+    
+    
+function n_of_n_stream() {
+    function helper(x,n,y) {
+        if(n===0) {
+            return helper(x+1,y+1,y+1);
+        }
+       else {
+           return pair(x,()=>helper(x,n-1,y));
+       }
+        
+    }
+    
+    return helper(1,1,1);
+    
+    
+    
+}
+
+    
+display_list(eval_stream(n_of_n_stream(),20));   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
