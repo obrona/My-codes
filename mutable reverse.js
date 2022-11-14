da80aa95d28de3efeda24f3c35765251c255dd64
Source Academy User <No public email provided>
2022-11-14 20:50:29 +0800

    Changes made from Source Academy

--- mutable reverse.js
+++ mutable reverse.js
@@ -37,6 +37,54 @@ function d_reverse(xs) {
     
 }
 }
+
+
+function f(xs) {
+    if(is_null(xs)||is_null(tail(xs))) {
+        return xs;
+    }
+
+    else {
+        const len = length(xs);
+        const wish = f(tail(xs));
+        set_tail(xs,null);
+        set_tail(tail_n_times(wish,len-2),xs);
+        return wish;
+    }
+    
+}
+
+function h(xs){
+  let s = null;
+  //let temp = xs;
+  const len = length(xs);
+  for(let i=0;i<len;i=i+1) {
+ 
+ 
+   
+        display(s);
+        //temp = tail(xs);
+        const d = xs;
+        xs = tail(xs);
+    
+        set_tail(d,s);
+        s=d;
+        
+    
+    
+    
+    
+}
+   return s;
+
+}
+
+
 const a = list(1,2,3,4,5);
-d_reverse(a);
-//a;
+h(a);
+a;
+
+
+
+
+
