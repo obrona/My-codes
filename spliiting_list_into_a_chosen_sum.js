32966c08096e66259c4b094587fdec07c38fd2bf
Source Academy User <No public email provided>
2022-11-19 13:33:46 +0800

    Changes made from Source Academy

---
+++ spliiting_list_into_a_chosen_sum.js
@@ -0,0 +1,53 @@
+
+function sum(xs) {
+    return is_null(xs) ? 0 : head(xs)+sum(tail(xs));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function split(xs,y) {
+    if(is_null(xs)) {
+        return y===0? list(list(),list()) : undefined;
+    }
+    else {
+       const a = split(tail(xs),y-head(xs));
+       const b = (a!==undefined) ? list(pair(head(xs),head(a)),head(tail(a))) : undefined;
+       if(b!==undefined) {
+           return b;
+       }
+       else {
+           const c = split(tail(xs),y);
+           const d = (c!==undefined) ? list(head(c),pair(head(xs),list_ref(c,1))) : undefined;
+         return d;
+       } 
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
+
+display_list(split(list(1,5,11,5),10));
+
+
+
+
+
+
+
+
+
+
